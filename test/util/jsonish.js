b4f41c6c54407e2c0c0ee71d2b487b059f8dad54
Matt Hinz <matt.h@pricewaiter.com>
2016-06-24 14:28:54 -0700

    Flesh out JSONish tests
    And fix a couple lint issues.

--- test/util/jsonish.js
+++ test/util/jsonish.js
@@ -6,14 +6,28 @@ describe('JSONish', () => {
     describe('parse', () => {
         const tests = [
             ['', undefined],
-            ['""', ""],
+            [undefined, undefined],
+            ['null', null],
+            ['""', ''],
+            ['{"foo":"bar"}', { foo: 'bar' }],
         ];
         tests.forEach(([input, expected]) =>
             it(`handles ${input}`, () =>
-                expect(JSONish.parse(input)).to.equal(expected)
+                expect(JSONish.parse(input)).to.deep.equal(expected)
             )
         );
     });
     describe('stringify', () => {
+        const tests = [
+            [undefined, ''],
+            [null, 'null'],
+            ['foo', '"foo"'],
+            [{ foo: 'bar' }, '{"foo":"bar"}'],
+        ];
+        tests.forEach(([input, expected]) =>
+            it(`handles ${input}`, () =>
+                expect(JSONish.stringify(input)).to.deep.equal(expected)
+            )
+        );
     });
-})
+});
